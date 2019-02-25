5bb0fd9057f25a38106c43340fcb64dedd3c7aff
duxiaopan <>
2019-02-25 11:57:40 +0800

    解决代码报错问题

--- app/src/main/java/com/goldze/mvvmhabit/ui/splash/SplashActivity.java
+++ app/src/main/java/com/goldze/mvvmhabit/ui/splash/SplashActivity.java
@@ -27,5 +27,9 @@ public class SplashActivity extends BaseActivity<ActivitySpalshBinding,SplashVie
         return ViewModelProviders.of(this).get(SplashViewModel.class);
     }
 
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        viewModel.startCountDown();
+    }
 }
--- app/src/main/java/com/goldze/mvvmhabit/ui/splash/SplashViewModel.java
+++ app/src/main/java/com/goldze/mvvmhabit/ui/splash/SplashViewModel.java
@@ -1,23 +1,20 @@
 package com.goldze.mvvmhabit.ui.splash;
 
+import android.annotation.SuppressLint;
 import android.app.Application;
+import android.databinding.ObservableField;
 import android.support.annotation.NonNull;
 
-import com.trello.rxlifecycle2.android.FragmentEvent;
-
-import org.reactivestreams.Subscription;
-
 import java.util.concurrent.TimeUnit;
-import java.util.function.Consumer;
 
 import io.reactivex.Flowable;
-import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Action;
+import io.reactivex.functions.Consumer;
 import me.goldze.mvvmhabit.base.BaseViewModel;
 import me.goldze.mvvmhabit.binding.command.BindingAction;
 import me.goldze.mvvmhabit.binding.command.BindingCommand;
+import me.goldze.mvvmhabit.utils.RxUtils;
 
 public class SplashViewModel extends BaseViewModel {
 
@@ -26,6 +23,8 @@ public class SplashViewModel extends BaseViewModel {
     public SplashViewModel(@NonNull Application application) {
         super(application);
     }
+
+    public ObservableField<String> countDownContent = new ObservableField<>("跳过 3");
 
     //跳转到首页的点击事件, 逻辑从View层转换到ViewModel层
     public BindingCommand jumpToHomeOnClickCommand = new BindingCommand(new BindingAction() {
@@ -36,22 +35,20 @@ public class SplashViewModel extends BaseViewModel {
     });
 
     public void startCountDown() {
-        // 倒计时 10s
-        mDisposable = Flowable.intervalRange(0, 11, 0, 1, TimeUnit.SECONDS)
+        // 倒计时 3s
+        Flowable.intervalRange(1, 4, 1, 1, TimeUnit.SECONDS)
                 .observeOn(AndroidSchedulers.mainThread())
-                .doOnNext(new Consumer<Long>() {
+                .compose(RxUtils.bindToLifecycle(getLifecycleProvider()))//界面关闭自动取消
+                .subscribe(new Consumer<Long>() {
                     @Override
                     public void accept(Long aLong) throws Exception {
-                        tvCountDown.setText("跳过 " + String.valueOf(10 - aLong));
+                        if((3 - aLong)>=0){
+                            countDownContent.set("跳过 " + String.valueOf(3 - aLong));
+                        }else{
+                            finish();
+                        }
                     }
-                })
-                .doOnComplete(new Action() {
-                    @Override
-                    public void run() throws Exception {
-
-                    }
-                })
-                .subscribe();
+                });
     }
 
 
