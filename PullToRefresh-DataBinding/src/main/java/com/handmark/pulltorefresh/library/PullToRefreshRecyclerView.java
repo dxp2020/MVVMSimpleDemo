3f8d4e074b7ada18bfddefddefd67fbccd759746
duxiaopan <>
2019-05-07 13:39:00 +0800

    调整下拉刷新、上拉加载控件

---
+++ PullToRefresh-DataBinding/src/main/java/com/handmark/pulltorefresh/library/PullToRefreshRecyclerView.java
@@ -0,0 +1,142 @@
+package com.handmark.pulltorefresh.library;
+
+
+import android.content.Context;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+public class PullToRefreshRecyclerView extends PullToRefreshBase<RecyclerView> {
+
+    public PullToRefreshRecyclerView(Context context) {
+        super(context);
+    }
+
+    public PullToRefreshRecyclerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PullToRefreshRecyclerView(Context context, Mode mode) {
+        super(context, mode);
+    }
+
+    public PullToRefreshRecyclerView(Context context, Mode mode, AnimationStyle style) {
+        super(context, mode, style);
+    }
+
+    @Override
+    public final Orientation getPullToRefreshScrollDirection() {
+        return Orientation.VERTICAL;
+    }
+
+    @Override
+    protected RecyclerView createRefreshableView(Context context, AttributeSet attrs) {
+        RecyclerView recyclerView = new RecyclerView(context, attrs);
+        return recyclerView;
+    }
+
+    @Override
+    protected boolean isReadyForPullStart() {
+        return isFirstItemVisible();
+    }
+
+    @Override
+    protected boolean isReadyForPullEnd() {
+        return isLastItemVisible();
+    }
+
+    /**
+     * 判断第一个条目是否完全可见
+     */
+    private boolean isFirstItemVisible() {
+        final RecyclerView.Adapter<?> adapter = getRefreshableView().getAdapter();
+
+        // 如果未设置Adapter或者Adapter没有数据可以下拉刷新
+        if (null == adapter || adapter.getItemCount() == 0) {
+            if (DEBUG) {
+                Log.d(LOG_TAG, "isFirstItemVisible. Empty View.");
+            }
+            return true;
+
+        } else {
+            // 第一个条目完全展示,可以刷新
+            if (getFirstVisiblePosition() == 0) {
+                return mRefreshableView.getChildAt(0).getTop() >= mRefreshableView
+                        .getTop();
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * 获取第一个可见子View的位置下标
+     */
+    private int getFirstVisiblePosition() {
+        View firstVisibleChild = mRefreshableView.getChildAt(0);
+        return firstVisibleChild != null ? mRefreshableView
+                .getChildAdapterPosition(firstVisibleChild) : -1;
+    }
+
+    /**
+     * 判断最后一个条目是否完全可见
+     */
+    private boolean isLastItemVisible() {
+        final RecyclerView.Adapter<?> adapter = getRefreshableView().getAdapter();
+
+        // 如果未设置Adapter或者Adapter没有数据可以上拉刷新
+        if (null == adapter || adapter.getItemCount() == 0) {
+            if (DEBUG) {
+                Log.d(LOG_TAG, "isLastItemVisible. Empty View.");
+            }
+            return true;
+
+        } else {
+            // 最后一个条目View完全展示,可以刷新
+            int lastVisiblePosition = getLastVisiblePosition();
+            if(lastVisiblePosition >= mRefreshableView.getAdapter().getItemCount()-1) {
+                return mRefreshableView.getChildAt(
+                        mRefreshableView.getChildCount() - 1).getBottom() <= mRefreshableView
+                        .getBottom();
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * 获取最后一个可见子View的位置下标
+     */
+    private int getLastVisiblePosition() {
+        View lastVisibleChild = mRefreshableView.getChildAt(mRefreshableView
+                .getChildCount() - 1);
+        return lastVisibleChild != null ? mRefreshableView
+                .getChildAdapterPosition(lastVisibleChild) : -1;
+    }
+
+    public static class WrapContentLinearLayoutManager extends LinearLayoutManager {
+        public WrapContentLinearLayoutManager(Context context) {
+            super(context);
+        }
+
+        public WrapContentLinearLayoutManager(Context context, int orientation, boolean reverseLayout) {
+            super(context, orientation, reverseLayout);
+        }
+
+        public WrapContentLinearLayoutManager(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+            super(context, attrs, defStyleAttr, defStyleRes);
+        }
+
+        @Override
+        public void onLayoutChildren(RecyclerView.Recycler recycler, RecyclerView.State state) {
+            try {
+                super.onLayoutChildren(recycler, state);
+            } catch (IndexOutOfBoundsException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
